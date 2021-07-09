6d51cd4fac2605bb05f175831c1a351583f3ce2d
aldanajuansebastian <aldanajuansebastian1@gmail.com>
2021-07-09 11:47:59 -0500

    List responsive and span on inicio.js added

--- src/Inicio.js
+++ src/Inicio.js
@@ -6,10 +6,9 @@ function Inicio (props) {
     return (
         <div className="inicio">
             <div className="contenedor">
-                <img className="logog" src="img/logocp.png" width="80%"/> 
+                <img className="logog" src="img/logocp.png" width="80%" alt="Logo Cinepelis"/> 
                 <h2>CINE INDEPENDIENTE</h2>
-                <h3>¡Disfruta de los mejores filmes <br/> independientes producidas por</h3>
-                <img className="logop" src="img/logocp.png" width="30%"/> 
+                <h3>¡Disfruta de los mejores filmes <br/> independientes producidas por <span id="fontCP">CinePelis</span></h3>
                 <br/>
                 <button className="btn-blue" type="button"><Link to="buscar" smooth={true} duration={1000}>Buscar</Link></button>
             </div>
